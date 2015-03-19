47ab8308352d91b713941e64da38add7378fd58b
Damian Kęska <webnull.www@gmail.com>
2015-03-19 23:04:34 +0100

    Added test for issue #139

---
+++ test/issues/Issue139Test.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Testcase for issue #139
+ *
+ * @link https://github.com/rainphp/raintpl3/issues/139
+ * @author Damian Kęska <damian.keska@fingo.pl>
+ */
+class Issue139Test extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Testcase for issue #139
+     *
+     * @link https://github.com/rainphp/raintpl3/issues/139
+     * @author Damian Kęska <damian.keska@fingo.pl>
+     */
+    public function testIssue139()
+    {
+        Rain\Tpl::configure(array(
+            'debug' => true,
+            'tpl_dir' => '/tmp/',
+            'cache_dir' => '/tmp/',
+        ));
+
+        $tpl = new \Rain\Tpl();
+        $tpl->assign('array', array(
+            1, 2, 3,
+        ));
+
+        $this->assertEquals('4', $tpl->drawString('{function="(count($array) + 1)"}', true));
+        $this->assertEquals('4', $tpl->drawString('{function="(count($array)+1)"}', true));
+        $this->assertEquals('4', $tpl->drawString('{function="(1 + count($array))"}', true));
+        $this->assertEquals('4', $tpl->drawString('{count($array) + 1}', true));
+        $this->assertEquals('4', $tpl->drawString('{count($array)+1}', true));
+    }
+}
