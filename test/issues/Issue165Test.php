c359ffa652685ebd09013d8ada7bc6624a52d8c6
Damian Kęska <webnull.www@gmail.com>
2015-03-19 22:49:48 +0100

    Added test for issue #165

---
+++ test/issues/Issue165Test.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Test case for issue #165
+ *
+ * @link https://github.com/rainphp/raintpl3/issues/165
+ * @author Damian Kęska <damian@pantheraframework.org>
+ */
+class Issue165Test extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test case for issue #165
+     *
+     * @link https://github.com/rainphp/raintpl3/issues/165
+     * @author Damian Kęska <damian@pantheraframework.org>
+     */
+    public function testIssue165()
+    {
+        Rain\Tpl::configure(array(
+            'debug' => true,
+            'tpl_dir' => '/tmp/',
+            'cache_dir' => '/tmp/',
+        ));
+
+        $tpl = new \Rain\Tpl();
+        $this->assertEquals('//cdn.my-project.com/assets/js/jquery.js', $tpl->drawString("{\$requirejs_main='//cdn.my-project.com/assets/js/jquery.js'}{\$requirejs_main}", true));
+        $this->assertEquals('//cdn.my-project.com/assets/js/jquery.js', $tpl->drawString('{$requirejs_main="//cdn.my-project.com/assets/js/jquery.js"}{$requirejs_main}', true));
+    }
+}
