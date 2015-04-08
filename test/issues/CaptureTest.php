e0da44e2bcf69aafdcf62d3f4b88631f7f801677
Damian Kęska <webnull.www@gmail.com>
2015-04-08 22:42:59 +0200

    Added some test cases for {capture} tag

---
+++ test/issues/CaptureTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * {capture}, {print}, {autoescape} tags test
+ *
+ * @author Damian Kęska <damian@pantheraframework.org>
+ */
+class CaptureTest extends RainTPLTestCase
+{
+
+    /**
+     * Base test
+     *
+     * @author Damian Kęska <damian@pantheraframework.org>
+     */
+    public function testBaseCaptureBlock()
+    {
+        $this->setup();
+        $this->assertEquals('This is a capture tag test', $this->engine->drawString("{capture name=\"test\"}This is a capture tag test{/capture}{\$test}", true));
+    }
+
+    /**
+     * Include a print="true" argument
+     *
+     * @author Damian Kęska <damian@pantheraframework.org>
+     */
+    public function testBaseCaptureBlockWithPrint()
+    {
+        $this->setup();
+        $this->assertEquals('This is a capture tag test', $this->engine->drawString("{capture name=\"test\" print=\"true\"}This is a capture tag test{/capture}", true));
+    }
+
+    /**
+     * Use filter="trim" (to filter output using trim() function)
+     *
+     * @author Damian Kęska <damian@pantheraframework.org>
+     */
+    public function testCaptureBlockWithTrimFilter()
+    {
+        $this->setup();
+        $this->assertEquals('This is a capture tag test', $this->engine->drawString("{capture name=\"test\" print=\"true\" filter=\"trim\"} This is a capture tag test {/capture}", true));
+    }
+
+    /**
+     * Use filter="substr", filterArg="0" and filterArg2="8" (to cut off output) which should compile to substr($output, 0, 8)
+     *
+     * @author Damian Kęska <damian@pantheraframework.org>
+     */
+    public function testCaptureBlockWithSubstrFilterAndArguments()
+    {
+        $this->setup();
+        $this->assertEquals(' This is', $this->engine->drawString("{capture name=\"test\" print=\"true\" filter=\"substr\" filterArg1=\"0\" filterArg2=\"8\"} This is a capture tag test {/capture}", true));
+    }
+}
