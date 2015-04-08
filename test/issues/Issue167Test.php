f011c0d3a32044945bdc171b3d005db34b2abd61
Damian Kęska <webnull.www@gmail.com>
2015-04-08 21:54:45 +0200

    Fixed issue #167 testcase

--- test/issues/Issue167Test.php
+++ test/issues/Issue167Test.php
@@ -11,12 +11,12 @@ class Issue167Test extends PHPUnit_Framework_TestCase
 
         $tpl = new \Rain\Tpl();
         $tpl->assign('timestamp', strtotime('2015-03-02 10:30'));
-        $this->assertEquals('02-03-2015', $tpl->drawString("2015-03-02 - {'d-m-Y'|date:\$timestamp}", true));
+        $this->assertEquals('2015-03-02 - 02-03-2015', $tpl->drawString("2015-03-02 - {'d-m-Y'|date:\$timestamp}", true));
 
         $tpl->assign('timestamp', strtotime('2015-03-03 10:30'));
-        $this->assertEquals('03-03-2015', $tpl->drawString("2015-03-03 - {\"d-m-Y\"|date:\"\$timestamp\"}", true));
+        $this->assertEquals('2015-03-03 - 03-03-2015', $tpl->drawString("2015-03-03 - {\"d-m-Y\"|date:\"\$timestamp\"}", true));
 
         $tpl->assign('timestamp', strtotime('2015-03-04 10:30'));
-        $this->assertEquals('04-03-2015', $tpl->drawString("2015-03-04 - {'d-m-Y'|date:\"\$timestamp\"}", true));
+        $this->assertEquals('2015-03-04 - 04-03-2015', $tpl->drawString("2015-03-04 - {'d-m-Y'|date:\"\$timestamp\"}", true));
     }
 }
