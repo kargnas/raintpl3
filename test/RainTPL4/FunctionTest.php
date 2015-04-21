afa962f8890944b2e1644f8dec5fffa377cd0047
Damian Kęska <webnull.www@gmail.com>
2015-04-21 17:38:34 +0200

    Added FunctionTest PHPUnit testcase

--- test/RainTPL4/FunctionTest.php
+++ test/RainTPL4/FunctionTest.php
@@ -7,7 +7,7 @@
 class FunctionTest extends RainTPLTestCase
 {
     /**
-     * {function="Date( 'c', $value.Time )"}">{$value.Time|FullDate}
+     * <code>{function="Date( 'c', $value.Time )"}">{$value.Time|FullDate}</code>
      *
      * @author Damian Kęska <damian@pantheraframework.org>
      */
@@ -18,7 +18,35 @@ class FunctionTest extends RainTPLTestCase
             'Time' => strtotime('2015-05-05 10:00'),
         ));
         //$this->engine->setConfigurationKey('print_parsed_code', true);
-        $this->assertEquals('', $this->engine->drawString('{function="Date( \'c\', $value.Time )"}">{$value.Time|FullDate}', true));
+        $this->assertEquals('2015-05-05T10:00:00+02:00">2015-05-05 10:00', $this->engine->drawString($this->getTestCodeFromPHPDoc(), true));
+    }
+
+    /**
+     * <code>
+     * {loop="$app_history"}
+     *     {function="Date( 'c', $value.Time )"}">{$value.Time|FullDate}
+     * {/loop}
+     * </code>
+     *
+     * <expects>2015-05-05T10:00:00+02:00">2015-05-05 10:00</expects>
+     *
+     * @author xPaw <github@xpaw.me>
+     * @author Damian Kęska <damian@pantheraframework.org>
+     */
+    public function testXPawFunctionVarReplace()
+    {
+        $this->setupRainTPL4();
+        $this->engine->assign('app_history', array(
+            array(
+                'Time' => strtotime('2015-05-05 10:00'),
+            ),
+        ));
+
+        //$this->engine->setConfigurationKey('print_parsed_code', true);
+        $this->assertEquals(
+            trim($this->getExpectationsFromPHPDoc()),
+            trim($this->engine->drawString($this->getTestCodeFromPHPDoc(), true))
+        );
     }
 }
 
